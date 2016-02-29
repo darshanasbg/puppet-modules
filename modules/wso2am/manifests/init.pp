7247cb274496bd1e8c4a6a696ccda7295bfd4409
Abimaran Kugathasan <abimaran@wso2.com>
2016-02-29 12:53:12 -0500

    Added configuration for custom jwt token generation

--- modules/wso2am/manifests/init.pp
+++ modules/wso2am/manifests/init.pp
@@ -26,6 +26,7 @@ class wso2am inherits wso2base {
   $apim_store         = hiera_hash ("wso2::apim_store")
   $third_party_keymanager = hiera("wso2::third_party_keymanager")
   $map_existing_auth_apps = hiera("wso2::map_existing_auth_apps")
+  $jwt_token_generator_impl = hiera("wso2::jwt_token_generator_impl")
 
   wso2base::server { "${carbon_home}" :
     maintenance_mode   => $maintenance_mode,
